f3f4e596828cdcb57267c9c3763d2f99bdc827a4
Eduardo2006g <101937977+Eduardo2006g@users.noreply.github.com>
2023-03-08 23:31:49 -0300

    Update dogsanalyse.py

--- dogsanalyse.py
+++ dogsanalyse.py
@@ -1,23 +1,29 @@
 import numpy as np
 import tensorflow as tf
 
-# salve a imagem que deseja verificar como "cachorro.jpg"
-
-# carrega a imagem
 img = tf.keras.preprocessing.image.load_img('cachorro.jpg', target_size=(224, 224))
 
-# converte a imagem em um array numpy
 img_array = tf.keras.preprocessing.image.img_to_array(img)
 
-# adiciona uma dimensão extra para representar o lote de imagens (batch)
 img_array = np.expand_dims(img_array, axis=0)
 
-# normaliza os valores dos pixels para estarem entre 0 e 1
 img_array /= 255.0
 
-# faz a previsão com o modelo
 model = tf.keras.models.load_model('modelo_cachorro.h5')
 prediction = model.predict(img_array)
 
-# exibe a previsão
-print('Este é um(a): ', prediction)
+prediction = prediction.flatten()
+
+maior = 0
+nome = ''
+
+with open('classes.txt', 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+for i in range(len(prediction)):
+    if prediction[i] > maior:
+        nome = classes[i]
+        maior = prediction[i]
+
+print('Este é um(a):', nome)
+print('Com probabilidade de: ±{:.2f}%'.format(maior * 100))
